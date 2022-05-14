88f44c36d03781e46c4382267329cd8c0743058b
yAnselmus <eeromakinen@hotmail.com>
2022-05-14 20:27:49 +0300

    health check: should pass

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 })))
 
 //asdf
+//asf
 
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
